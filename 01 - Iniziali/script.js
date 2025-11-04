c31bb8071664ce8839aa068d01cc8e24ed824b1a
Wissembrahem <wbrahem19@gmail.com>
2025-11-04 15:58:09 +0100

    first

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,13 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
-
+function estraiPrimeLettere(names) {
+    const result = names.map(parola => parola[0]);
+        return result;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+const primeLettere = estraiPrimeLettere(names);
+console.log(primeLettere)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
