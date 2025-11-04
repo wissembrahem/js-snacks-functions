3c0a1b821e4c3198bebe651654a618e9403aae77
Wissembrahem <wbrahem19@gmail.com>
2025-11-04 17:57:33 +0100

    termine terzo esercizio

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,12 +1,30 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+const word = 'wissem';
+const vocali = ["a", "e", "i", "o", "u"];
 
 
 // Dichiara la funzione qui.
+function estrarre(word,vocali) {
+    let results = [];
+    for (let i = 0; i < word.length; i++) {
+        const lettera = word[i];
+        for (let i = 0; i < vocali.length; i++) {
+            const vocale = vocali[i];
+            if (vocale === lettera) {
+                results.push(lettera);
+            }
+        }
+    }
+    
+    return results
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const output = estrarre(word, vocali);
+
+console.log(output);
 
 
 
--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,17 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+const names = 'Mario';
 
 
 // Dichiara la funzione qui.
+function saluta(names) {
+    console.log( + names);
+    
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+saluta(names)
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
